0071bbb1f3097035a092f4273e4f3a12cd17bbde
manfred.madelaine <manfred.madelaine@outlook.fr>
2021-03-27 20:52:02 +0100

    :rocket: MAJOR: Stable version !

---
+++ src/main/java/com/shool/school/SchoolApplication.java
@@ -0,0 +1,21 @@
+package com.shool.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolApplication.class, args);
+	}
+
+	@GetMapping
+	public String hey(){
+		return "Hello sir !";
+	}
+
+}
